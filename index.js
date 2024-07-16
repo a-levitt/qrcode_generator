1cd8751614f22c98940fe394e83a27baa8aae164
Artemis Levitt <artemis.levitt@gmail.com>
2024-07-16 12:57:21 +0200

    working project

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ import inquirer from 'inquirer';
 import qr from 'qr-image';
 import fs from 'fs';
 
-// let qr_svg = qr.image('I love QR!', { type: 'svg' });
-// qr_svg.pipe(fs.createWriteStream('i_love_qr.svg'));
-// let svg_string = qr.imageSync('I love QR!', { type: 'svg' });
-
-// Input - {type: 'input'}
 inquirer
   .prompt([{
     message: "Type in a URL: ", 
-     name: "URL"
+    name: "URL"
   }])
   .then((answers) => {
-    console.log(answers);
+    const url = answers.URL;
+    let qr_img = qr.image( url, { type: 'png' });
+    qr_img.pipe(fs.createWriteStream('QRcodes/yourQR.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
